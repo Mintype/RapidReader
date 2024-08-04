91b2bf2964f620d73423d06f7a36e620dd447a98
Mintype <96578565+Mintype@users.noreply.github.com>
2024-08-03 22:08:46 -0400

    Change bold from half to just one character

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ paragraphs.forEach(paragraph => {
     const words = paragraph.textContent.split(' ');
     const formattedWords = words.map(word => {
         if (word.length > 1) {
-            const halfLength = Math.ceil(word.length / 2);
-            return `<b>${word.slice(0, halfLength)}</b>${word.slice(halfLength)}`;
+            // const halfLength = Math.ceil(word.length / 2);
+            return `<b>${word.slice(0, 1)}</b>${word.slice(1)}`;
         }
         return word;
     });
